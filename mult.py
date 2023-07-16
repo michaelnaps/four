9cf8650b4ec260e1e5f83893b7ccb207011528c0
Michael Napoli <mikednapoli@gmail.com>
2023-07-16 16:50:31 -0400

    simulated drawing successfully!

--- mult.py
+++ mult.py
@@ -13,7 +13,36 @@ if __name__ == "__main__":
 
     # Extract and form data vectors.
     Nx = len( points  );
-    T = np.array( [[i for i in range( Nx )]] );
+    T = np.array( [ [i for i in range( Nx )] ] );
     X = points[:,0].reshape( 1,Nx );
     Y = points[:,1].reshape( 1,Nx );
-    XY = np.vstack( (X, Y) );
+
+    # Initialize Transform variables.
+    fxvar = RealFourier( T, X );
+    fyvar = RealFourier( T, Y );
+
+    # Solve the DFT Problem.
+    fxvar.dft();
+    fyvar.dft();
+
+    # print( 'X:' );
+    # print( '\tA:', fxvar.A );
+    # print( '\tB:', fxvar.B );
+    # print( 'Y:' );
+    # print( '\tA:', fyvar.A );
+    # print( '\tB:', fyvar.B );
+
+    # Compute comparison vectors.
+    dt = 0.01;
+    Nf = round( Nx/dt );
+    Tf = np.array( [[ i*dt for i in range( Nf ) ]] );
+    Xf = fxvar.solve( Tf );
+    Yf = fyvar.solve( Tf );
+
+    # Plot results.
+    fig, axs = plt.subplots();
+    axs.plot( X.T, Y.T, label='Drawing' );
+    axs.plot( Xf.T, Yf.T, linestyle='--', label='Fourier' );
+    plt.legend();
+    plt.grid( 1 );
+    plt.show();
