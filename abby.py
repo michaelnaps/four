23c408efc1951a7e036ad322d16b61f0428eb2ca
Michael Napoli <mikednapoli@gmail.com>
2023-07-17 19:17:09 -0400

    problem was misshapen data and odd number of data points

--- abby.py
+++ abby.py
@@ -11,26 +11,27 @@ if __name__ == "__main__":
     data = pd.read_csv( datafile );
 
     # Data sets fror each object.
-    Xlist = (
+    Xlist = [
         data[ data['z']=='a' ].to_numpy()[:,:2].T,
         data[ data['z']=='b' ].to_numpy()[:,:2].T,
         data[ data['z']=='c' ].to_numpy()[:,:2].T,
         data[ data['z']=='d' ].to_numpy()[:,:2].T
-    );
+    ];
 
     # Size of data sets.
     Nlist = [ X.shape[1] for X in Xlist ];
-    print( Nlist );
+    for i, N in enumerate( Nlist ):
+        if N % 2 != 0:
+            Nlist[i] = N - 1;
+            Xlist[i] = Xlist[i][:,:-1];
 
     # Generate time series.
     Tlist = [ np.array( [[i for i in range( N )]] ) for N in Nlist ];
 
     # Fourier series lists.
-    flist = [ RealFourier( X, T ) for X, T in zip( Xlist, Tlist ) ];
+    flist = [ RealFourier( T, X ) for X, T in zip( Xlist, Tlist ) ];
     for fvar in flist:
         fvar.dft();
-        print( 1 );
-
 
     # # Plot data.
     # fig, axs = plt.subplots();
