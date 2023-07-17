2833f97cc7b5351fa61ad653a4afdde02bb73e5a
Michael Napoli <mnapoli@irobot.com>
2023-07-17 12:08:55 -0400

    smaller time step and trace

--- mult.py
+++ mult.py
@@ -34,21 +34,23 @@ if __name__ == "__main__":
     # Tf = np.array( [[ i*dt for i in range( Nf ) ]] );
     # XYf = fvar.solve( Tf );
 
-    # # Plot results.
-    # fig, axs = plt.subplots();
-    # axs.plot( X.T, Y.T, label='Drawing' );
+    # Plot results.
+    fig, axs = plt.subplots();
+    axs.plot( X.T, Y.T, label='Drawing' );
     # axs.plot( XYf[0], XYf[1], linestyle='--', label='Fourier' );
     # plt.legend();
     # plt.grid( 1 );
     # plt.show();
 
     # Initialize vehicle.
+    dt = 0.1;
     t = np.array( [[0]] );
     x = fvar.solve( t );
-    vhc = Vehicle2D( fvar.solve, x, radius=10, tail_length=300 );
+    vhc = Vehicle2D( fvar.solve, x,
+        fig=fig, axs=axs, radius=10, tail_length=3000 );
     vhc.setLimits( xlim=(-50, 700), ylim=(-50, 500) );
     vhc.draw();
-    while t < 1000:
-        t += 1;
+    while t < 500:
+        t = t + dt;
         x = fvar.solve( t );
         vhc.update( x );
