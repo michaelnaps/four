b479693a62360bd524236ac0c7b7278a4cf55f5a
Michael Napoli <mikednapoli@gmail.com>
2024-08-28 20:45:41 -0400

    small notation changes'

--- initial/rand.py
+++ initial/rand.py
@@ -3,22 +3,25 @@ from args import *
 
 if __name__ == "__main__":
     # Generate random data set with equally spaced points.
-    Nx = 20  # chosen arbitrarily.
+    Nx = 20  # Chosen arbitrarily.
+    L = 10   # Chosen arbitrarily.
     dx = np.random.rand()
     X = np.array( [[i*dx for i in range( Nx )]] )
-    Y = 2*np.random.rand( 1,Nx ) - 1
-    Xdft = np.linspace(0,dx*(Nx-1),1000).reshape( 1,1000 )
+    Y = 2*L*np.random.rand( 1,Nx ) - L
 
     # Results from class solver.
     fvar = RealFourier( X, Y )
     fvar.dft()
     print( fvar )
-    Ysolver = fvar.solve( Xdft )
+
+    # Solve over range.
+    Xdft = np.linspace( 0, dx*(Nx - 1), 1000 )[None]
+    Ydft = fvar.solve( Xdft )
 
     # Plot results.
     fig, axs = plt.subplots()
     plt.plot( X.T, Y.T, marker='o', label='true' )
-    plt.plot( Xdft.T, Ysolver.T, linestyle=':', label='solver' )
+    plt.plot( Xdft.T, Ydft.T, linestyle=':', label='solver' )
     plt.legend()
     plt.grid( 1 )
     plt.show()
