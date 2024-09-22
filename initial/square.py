a7a9314bbd479cff82a2d416b28356ec415d9425
Michael Napoli <mikednapoli@gmail.com>
2024-09-21 21:08:20 -0400

    added dft and ptb functions to square for testing

--- initial/square.py
+++ initial/square.py
@@ -4,7 +4,7 @@ from args import *
 # Hyper parameter(s).
 Nmax = 20
 dN = 4
-beta = 0.01
+beta = 1e-3
 
 # Square wave initialization.
 def wave(x):
@@ -17,18 +17,28 @@ if __name__ == '__main__':
     Y = wave( X )
 
     # Initialize solver variable.
-    fvar = RealFourier( X, Y )
+    fdft = RealFourier( X, Y ).dft().powerspec()
+    # fptb = perturbseries( fdft, eps=5e-1 )
 
     # Plot results.
     fig, axs = plt.subplots()
     axs.plot( X.T, Y.T, color='r', label='Model' )
 
+    Xf = np.linspace( -T, T, 1000 )[None]
+    axs.plot( Xf.T, fdft.solve( Xf ).T, color='k', label='DFT' )
+    # axs.plot( Xf.T, fptb.solve( Xf ).T, label='DFT perturbed' )
+
+    fvar = RealFourier( X, Y )
     for n in range( 0, Nmax+1, dN ):
         if n == 0:  n = n + 1;
         fvar.dmd( N=n )
         Yf = fvar.solve( X )
         axs.plot( X.T, Yf.T, linestyle=None, label=('N=%i' % n) )
 
-    plt.grid( 1 )
-    plt.legend()
+    axs.grid( 1 )
+    axs.legend()
+
+    fig2, axs2 = plt.subplots()
+    axs2.plot( fdft.F, fdft.R.T )
+
     plt.show()
