41e1e0a21bf4fcfb720a1ac2ddb0b83aaa5edee3
Michael Napoli <mikednapoli@gmail.com>
2026-05-23 23:27:18 -0400

    testing simple wavelet implementation

--- wavelet.jl
+++ wavelet.jl
@@ -6,15 +6,33 @@ function morlet(ω::defFloat; ω0::defFloat=5.0)::defFloat
     return (1/π)^(0.25)*exp( -0.5*(ω - ω0)^2 )
 end
 
-# Scaling transform.
-function scale(s::defFloat, ω::defFloat; ω0::defFloat=0.6, wavelet::Function=morlet)::defFloat
-    return √s.*wavelet( s*ω; ω0=ω0 )
+# Scaling function.
+function scale(F::ComplexFourier, s::defFloat; wavelet::Function=morlet)::Vector{defComp}
+    # Compute the scaled wavelet in the frequency domain.
+    φ = √s.*conj.( wavelet.( s.*F.ω ) )
+    return F.C.*φ
 end
 
-# Class functions.
-# mutable struct Wavelet
-#     # Fourier modes.
-#     M::FourierModes
-# end
+mutable struct Wavelet
+    # Fourier variable.
+    F::ComplexFourier
+
+    # Wavelet "mother" function.
+    φ::Function
+end
+
+Base.broadcastable(W::Wavelet) = Ref( W )
+
+function Wavelet(F::ComplexFourier; φ::Function=morlet)::Wavelet
+    return Wavelet( F, φ )
+end
+
+function solve(W::Wavelet, s::defFloat; wavelet::Function=morlet)::Vector{defFloat}
+    # Compute the scaled wavelet coefficients.
+    Ĉ = scale( W.F, s; wavelet=wavelet )
+
+    # Solve the wavelet coefficients.
+    return solve( ComplexFourier( W.F.M, Ĉ, W.F.X, W.F.Y, W.F.ω ); X=W.F.X )
+end
 
 println( "Loaded wavelet.jl class file." )
