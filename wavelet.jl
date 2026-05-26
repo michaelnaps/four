7d25d4497ffcd696b540304408dbf23a64786168
Michael Napoli <mikednapoli@gmail.com>
2026-05-25 21:48:02 -0400

    try looking at the wavelet spectrum using abs not pow

--- wavelet.jl
+++ wavelet.jl
@@ -2,17 +2,10 @@
 include( "power.jl" )
 
 # Wavelet functions.
-function morlet(ω::defFloat; ω0::defFloat=5.0)::defFloat
-    σ = (1 + exp( -ω0^2 ) - 2exp( -0.75*ω0^2 ) )^(-0.5)
+function morlet(ω::defFloat; ω0::defFloat=6.0)::defFloat
+    σ = (1 + exp( -ω0^2 ) - 2*exp( -0.75*ω0^2 ) )^(-0.5)
     κ = exp( -0.5*ω0^2 )
     return σ*(1/π)^(0.25)*(exp( -0.5*(ω - ω0)^2 ) - κ*exp( -0.5*ω^2 ))
-end
-
-# Scaling function.
-function scale(F::ComplexFourier, s::defFloat; wavelet::Function=morlet)::Vector{defComp}
-    # Compute the scaled wavelet in the frequency domain.
-    φ = √s.*conj.( wavelet.( s.*F.ω ) )
-    return F.C.*φ
 end
 
 mutable struct Wavelet
@@ -30,10 +23,15 @@ function Wavelet(F::ComplexFourier; φ::Function=morlet)::Wavelet
     return Wavelet( F, φ )
 end
 
-function solve(W::Wavelet, s::defFloat;
-    wavelet::Function=morlet, X::Vector{defFloat}=W.F.X)::Vector{defFloat}
+function scale(W::Wavelet, s::defFloat)::Vector{defComp}
+    # Compute the scaled wavelet in the frequency domain.
+    φ = √s.*conj.( W.φ.( s.*W.F.ω ) )
+    return W.F.C.*φ
+end
+
+function solve(W::Wavelet, s::defFloat; X::Vector{defFloat}=W.F.X)
     # Compute the scaled wavelet coefficients.
-    Ĉ = scale( W.F, s; wavelet=wavelet )
+    Ĉ = scale( W, s )
 
     # Solve the wavelet coefficients.
     return solve( ComplexFourier( W.F.M, Ĉ, W.F.X, W.F.Y, W.F.ω ); X=X )
